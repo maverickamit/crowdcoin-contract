5400973d1c38fdc93a48e39ba3bfa894b28dfaa6
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-01-07 12:14:45 +0530

    feat: implement contribute function - add ability to contribute money to the campaign - keep track of contributors - keep track of number of total number of unique contributers

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -5,9 +5,21 @@ contract Campaign {
     
     address public manager;
     uint public minimumContribution;
+    mapping(address => bool) public contributors;
+    uint public contributorsCount;
+
+
     
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    function contribute () public payable {
+        require(msg.value> minimumContribution);
+        if (!contributors[msg.sender]) {
+         contributorsCount++;
+        }
+      contributors[msg.sender] = true;
     }
 }
